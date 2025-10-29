ea87bbc39dedf742b6c7fd30b1dc1d5fe30ac831
melvin21github <melvintk09@gmail.com>
2025-10-29 22:06:28 +0100

    Added wheels to the robot with continous joint

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -1,28 +1,35 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    urdf_file = os.path.join(
-        os.getenv('HOME'),
-        'ros2_ws/src/my_robot_description/urdf/my_robot.urdf'
-    )
+    pkg_path = get_package_share_directory('my_robot_description')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'my_robot.urdf')
 
     with open(urdf_file, 'r') as infp:
         robot_desc = infp.read()
 
-    return LaunchDescription([
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': robot_desc}],
-        ),
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen',
-        ),
-    ])
+    # Publishes transforms from URDF
+    robot_state_pub = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_desc}],
+        output='screen'
+    )
+
+    # GUI sliders for joints
+    joint_state_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    # Visualize in RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen'
+    )
+
+    return LaunchDescription([robot_state_pub, joint_state_gui, rviz])
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 import os
 from glob import glob
 
@@ -7,27 +7,25 @@ package_name = 'my_robot_description'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
+        # register package
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+         ['resource/' + package_name]),
+
+        # install launch, urdf and rviz directories
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='melvin',
-    maintainer_email='melvintk09@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer_email='melvin@example.com',
+    description='Simple robot description package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
